a5dd72e3ca598841212fc391e33df3dee74da4d8
Jesse L Lawson <homestarflyer@gmail.com>
2016-09-28 15:54:59 -0400

    set up omniauth and facebook sign up

--- Gemfile
+++ Gemfile
@@ -34,7 +34,9 @@ gem 'toastr-rails'
 # gem 'capistrano-rails', group: :development
 
 gem 'sendgrid-rails', '~> 2.0', '>= 2.0.5'
-gem 'devise'
+gem "devise", :github => 'plataformatec/devise', :branch => 'master'
+gem 'omniauth'
+gem 'omniauth-facebook'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
@@ -44,6 +46,7 @@ group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console'
   gem 'listen', '~> 3.0.5'
+  gem 'pry'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable
+         :confirmable, :omniauthable # :omniauth_providers => [:facebook]
 
   validates :fullname, presence: true, length: {maximum: 50}
+
+  def self.from_omniauth(auth)
+    user = User.where(email: auth.info.email).first
+    if user
+      user
+    else
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+        user.email = auth.info.email
+        user.password = Devise.friendly_token[0,20]
+        user.fullname = auth.info.name
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.image = auth.info.image
+      end
+    end
+  end
+
 end
