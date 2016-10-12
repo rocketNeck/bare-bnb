df2d77e886b40a4c2f94bd9038102bdac21acddf
Jesse L Lawson <homestarflyer@gmail.com>
2016-10-12 13:21:03 -0400

    ajax for end_date

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -9,6 +9,17 @@ class ReservationsController < ApplicationController
     render json: reservations
   end
 
+  def preview
+    start_date = Date.parse(params[:start_date])
+    end_date = Date.parse(params[:end_date])
+
+    output = {
+      conflict: is_conflict(start_date, end_date)
+    }
+
+    render json: output
+  end
+
   def create
     @reservation = current_user.reservations.create(reservation_params)
     redirect_to @reservation.room, notice: "Your reservation has been created."
@@ -16,5 +27,10 @@ class ReservationsController < ApplicationController
   private
   def reservation_params
     params.require(:reservation).permit(:start_date, :end_date, :price, :total, :room_id)
+  end
+  def is_conflict(start_date, end_date)
+    room = Room.find(params[:room_id])
+    check = room.reservations.where("? < start_date AND end_date < ?", start_date, end_date)
+    check.size > 0 ? true : false
   end
 end
