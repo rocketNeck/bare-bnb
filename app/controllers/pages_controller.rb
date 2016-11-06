40bbd5580a24cb363cfa29a41479fd073f2ba591
Jesse L Lawson <homestarflyer@gmail.com>
2016-11-06 18:07:24 -0500

    added navbar search and fixed search page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,42 +1,97 @@
 class PagesController < ApplicationController
   def home
-    @rooms = Room.limit(6)
+  	@rooms = Room.all
   end
 
   def search
-    if params[:search].present? && params[:search].strip != ""
-      session[:loc_search] = params[:search]
-    end
 
-    arrRooms = Array.new
-    if session[:loc_search] && session[:loc_search] != ""
-      @rooms_address = Room.where(active: true).near(session[:loc_search], 5, order: 'distance')
-    else
-      @rooms_address = Room.where(active: true).all
-    end
+  	if params[:search].present? && params[:search].strip != ""
+  		session[:loc_search] = params[:search]
+  	end
 
-    @search = @rooms_address.ransack(params[:q])
-    @rooms = @search.result
+  	@arrRooms = Array.new
 
-    @arrRooms = @rooms.to_a
-    if (params[:start_date].present? && params[:end_date].present?)
-      start_date = Date.parse(params[:start_date])
-      end_date = Date.parse(params[:end_date])
+  	if session[:loc_search] && session[:loc_search] != ""
+  		@rooms_address = Room.where(active: true).near(session[:loc_search], 5, order: 'distance')
+  	else
+  		@rooms_address = Room.where(active: true).all
+  	end
 
-      @rooms.each do |room|
-        not_available = room.reservations.where(
-          "(? <= start_date AND start_date <= ?)
-          OR (? <= end_date AND end_date <= ?)
-          OR (start_date < ? AND ? end_date)",
-          start_date, end_date,
-          start_date, end_date,
-          start_date, end_date
-        ).limit(1)
+  	@search = @rooms_address.ransack(params[:q])
+  	@rooms = @search.result
 
-        if not_available.length > 0
-          @arrRooms.delete(room)
-        end
-      end
-    end
+  	@arrRooms = @rooms.to_a
+
+  	if (params[:start_date] && params[:end_date] && !params[:start_date].empty? & !params[:end_date].empty?)
+
+  		start_date = Date.parse(params[:start_date])
+  		end_date = Date.parse(params[:end_date])
+
+  		@rooms.each do |room|
+
+  			not_available = room.reservations.where(
+  					"(? <= start_date AND start_date <= ?)
+  					OR (? <= end_date AND end_date <= ?)
+  					OR (start_date < ? AND ? < end_date)",
+  					start_date, end_date,
+  					start_date, end_date,
+  					start_date, end_date
+  				).limit(1)
+
+  			if not_available.length > 0
+  				@arrRooms.delete(room)
+  			end
+
+  		end
+
+  	end
+
   end
 end
+
+
+#
+#
+# class PagesController < ApplicationController
+#   def home
+#     @rooms = Room.limit(6)
+#   end
+#
+#   def search
+#     if params[:search].present? && params[:search].strip != ""
+#       binding.pry
+#       session[:loc_search] = params[:search]
+#     end
+#
+#     arrRooms = Array.new
+#     if session[:loc_search] && session[:loc_search] != ""
+#       @rooms_address = Room.where(active: true).near(session[:loc_search], 5, order: 'distance')
+#     else
+#       @rooms_address = Room.where(active: true).all
+#     end
+#
+#     @search = @rooms_address.ransack(params[:q])
+#     @rooms = @search.result
+#
+#     @arrRooms = @rooms.to_a
+#     if (params[:start_date].present? && params[:end_date].present?)
+#       start_date = Date.parse(params[:start_date])
+#       end_date = Date.parse(params[:end_date])
+#
+#       @rooms.each do |room|
+#         not_available = room.reservations.where(
+#           "(? <= start_date AND start_date <= ?)
+#           OR (? <= end_date AND end_date <= ?)
+#           OR (start_date < ? AND ? end_date)",
+#           start_date, end_date,
+#           start_date, end_date,
+#           start_date, end_date
+#         ).limit(1)
+#
+#         if not_available.length > 0
+#           @arrRooms.delete(room)
+#         end
+#       end
+#     end
+#   end
+# end
